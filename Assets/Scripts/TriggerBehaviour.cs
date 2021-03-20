c5d93a64b843213a62a3edfdd86519be45e6246e
Matt <matthew.crees@ntlworld.com>
2021-03-20 15:55:44 +0000

    fixed trigger

--- Assets/Scripts/TriggerBehaviour.cs
+++ Assets/Scripts/TriggerBehaviour.cs
@@ -8,7 +8,7 @@ public class TriggerBehaviour : MonoBehaviour
     public GameObject parent;
 
     private void OnTriggerEnter(Collider other) {
-        Instantiate(RiverSegment, gameObject.GetComponentInParent<Transform>().position + new Vector3(0f, 0f, 220f), gameObject.GetComponentInParent<Transform>().rotation);
+        Instantiate(RiverSegment, gameObject.GetComponentInParent<Transform>().position + new Vector3(0f, 0f, 218f), gameObject.GetComponentInParent<Transform>().rotation);
         Invoke(nameof(Die), 15f);
     }
 
