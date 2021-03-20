9e3adfd06416ce46afea0347a80daf1adc3578a6
Matt <matthew.crees@ntlworld.com>
2021-03-20 15:08:04 +0000

    neverending river

---
+++ Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    public GameObject RiverSegment;
+    public GameObject parent;
+
+    private void OnTriggerEnter(Collider other) {
+        Instantiate(RiverSegment, gameObject.GetComponentInParent<Transform>().position + new Vector3(0f, 0f, 173.5f), gameObject.GetComponentInParent<Transform>().rotation);
+        Invoke(nameof(Die), 30f);
+    }
+
+    void Die() {
+        Destroy(parent);
+    }
+
+}
