593c6252ae3945d5a3057d686256642bca2d8301
Matt <matthew.crees@ntlworld.com>
2021-03-21 11:34:28 +0000

    submit score button

--- Assets/Scripts/GameOverBehaviour.cs
+++ Assets/Scripts/GameOverBehaviour.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Database;
+using UnityEngine.UI;
 
 public class GameOverBehaviour : MonoBehaviour
 {
+    public InputField ifield;
+
     public void Restart() {
-        DatabaseConnector.NewScore(Stats._username, Stats._score);
+        
 
         Stats._generations = 1;
         Stats._score = 0;
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+    }
+
+    public void SubmitScore() {
+        Stats._username = ifield.text;
+        DatabaseConnector.NewScore(Stats._username, Stats._score);
     }
 
     public void Continue() {
