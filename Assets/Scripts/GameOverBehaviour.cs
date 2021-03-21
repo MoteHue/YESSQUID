d433e9ef96fcf103b033cb77406702b02149247c
TBExtent <jedpreist@gmail.com>
2021-03-21 10:55:36 +0000

    scores

--- Assets/Scripts/GameOverBehaviour.cs
+++ Assets/Scripts/GameOverBehaviour.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Database;
 
 public class GameOverBehaviour : MonoBehaviour
 {
     public void Restart() {
+        DatabaseConnector.NewScore(Stats._username, Stats._score);
+
         Stats._generations = 1;
         Stats._score = 0;
         Scene scene = SceneManager.GetActiveScene();
