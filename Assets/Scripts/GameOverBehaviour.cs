513bdf3824b1d0a7c204068a199be06a68ba33a9
Matt <matthew.crees@ntlworld.com>
2021-03-21 10:06:47 +0000

    reset score and gen on restart

--- Assets/Scripts/GameOverBehaviour.cs
+++ Assets/Scripts/GameOverBehaviour.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class GameOverBehaviour : MonoBehaviour
 {
     public void Restart() {
+        Stats._generations = 1;
+        Stats._score = 0;
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
     }
